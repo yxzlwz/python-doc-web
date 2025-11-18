f945fefba44f1518a97440ddd2a06f8b775fd374
yxzlwz <yxzlwz@gmail.com>
2025-11-18 14:31:57 +0800

    update contributors

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -15,11 +15,8 @@ export default defineNavbarConfig([
             { text: "异想之旅", link: "https://blog.yxzl.dev/" },
             { text: "程家麒", link: "https://modenc.top/" },
             { text: "Honahec", link: "https://honahec.cc/" },
-            { text: "Yuzhen Qin", link: "https://github.com/YuzhenQin" },
             { text: "Iron_Grey_", link: "https://irongrey.top/" },
-            { text: "Faceybr0", link: "https://github.com/faceybro" },
-            { text: "Richard Yang", link: "https://github.com/yang-zhongtian" },
-            { text: "Cheng", link: "https://github.com/jackywwwwww" },
+            { text: "完整名单", link: "/taqbzfxf/" },
         ],
     },
 ]);
